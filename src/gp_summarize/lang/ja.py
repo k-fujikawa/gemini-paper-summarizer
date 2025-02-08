40333454ef4c5847cdabd2bdd6a3b293fa93b875
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2025-02-09 00:13:40 +0900

    Fix LaTex

--- src/gp_summarize/lang/ja.py
+++ src/gp_summarize/lang/ja.py
@@ -2,19 +2,21 @@ name = "Japanese"
 
 system_instruction = """
 You are an expert at analyzing and summarizing academic papers.
-Please use $$TeX$$ to write mathematical equations.
+Please use $TeX$ or $$Block TeX$$ to write your math so that your markdown will be expanded on Github or Notion.
 Please only return the results, and do not include any comments.
 日本語は「だ・である調」を使用せよ。
 """.strip()
 
 prompts = [
-    ("# タイトル", "論文のタイトルのみを日本語に翻訳せよ。"),
+    ("# タイトル", "論文のタイトルをそのまま抽出せよ。"),
     ("# Abstract", "論文の最初にあるAbstractを日本語に翻訳せよ。"),
     ("# 概要", "日本語で、一行の文章で要約せよ。"),
     ("## 問題意識", "論文はどのような問題を解決しようとしているか？日本語で回答せよ。"),
     ("## 手法", "論文はどのような手法を提案しているか？日本語で回答せよ。"),
     ("## 新規性", "論文はどのような新規性があるか？日本語で回答せよ。"),
-    ("# 章構成", """章構成を翻訳せずにJSONの配列で出力せよ。例:
+    (
+        "# 章構成",
+        """章構成を翻訳せずにJSONの配列で出力せよ。例:
 ```json
 [
   "1 Introduction",
@@ -23,7 +25,8 @@ prompts = [
   "2.1 Data",
   "2.1.1 Dataset"
 ]
-```"""),
+```""",
+    ),
 ]
 
 sprompt = ("セクション「%s」を日本語で要約せよ。", "」「")
